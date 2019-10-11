d3fc7d48c102d0bb6c9d3ad09e02554007ad3622
Nick Fosdick <nick@lark-it.com>
2019-10-10 20:48:12 -0600

    Adding init.pp

---
+++ setup_os/manifests/init.pp
@@ -0,0 +1,10 @@
+class setup_os {
+  dsc_registry {'deny_ts_connection':
+    dsc_ensure => 'Present',
+    dsc_key => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server',
+    dsc_valuename => 'DenyTSConnection',
+    dsc_valuedata => '0',
+    dsc_valuetype => 'Dword',
+    dsc_force     => true,
+  }
+}
