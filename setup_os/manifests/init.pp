9a075705c9809cc145af2d8cd005bea7e45224f9
Nick Fosdick <nick@lark-it.com>
2019-10-14 19:01:20 -0600

    multi-cast

--- setup_os/manifests/init.pp
+++ setup_os/manifests/init.pp
@@ -2,12 +2,20 @@ class setup_os {
 
   # https://lark-it.atlassian.net/browse/FCB-138
   dsc_registry {'deny_ts_connection':
-    dsc_ensure => 'Present',
-    dsc_key => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server',
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server',
     dsc_valuename => 'DenyTSConnection',
     dsc_valuedata => '0',
     dsc_valuetype => 'Dword',
     dsc_force     => true,
+  }
+
+  # https://lark-it.atlassian.net/browse/FCB-158
+  dsc_registry {'Disable Multicast':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters',
+    dsc_valuename => 'IGMPLevel',
+    dsc_valuedata => '0',
   }
 
   # https://lark-it.atlassian.net/browse/FCB-141
