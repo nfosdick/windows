f207ed821eacee4241fbc584bc397f2ae1351b4b
Nick Fosdick <nick@lark-it.com>
2019-10-14 19:55:17 -0600

    Adding disable TLS 1.0

--- setup_os/manifests/init.pp
+++ setup_os/manifests/init.pp
@@ -10,7 +10,20 @@ class setup_os {
     dsc_force     => true,
   }
 
+  dsc_registry {'Disable TLS 1.0':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server',
+    dsc_valuename => 'Enabled',
+    dsc_valuedata => '0',
+    dsc_valuetype => 'Dword',
+    dsc_force     => true,
+  }
+
   # https://lark-it.atlassian.net/browse/FCB-158
+  # https://www.microsoftpressstore.com/articles/article.aspx?p=2217263&seqNum=8
+  # https://docs.microsoft.com/en-us/powershell/module/nettcpip/set-netipv4protocol?view=win10-ps
+  # https://social.technet.microsoft.com/Forums/windows/en-US/0d0da348-3b8b-4790-8aa2-60740a02536f/windows-7-and-igmp-multicast?forum=w7itpronetworking
+  # https://superuser.com/questions/788347/how-do-i-disable-multicast-on-the-tcp-ip-stack-for-windows
   dsc_registry {'Disable Multicast':
     dsc_ensure    => 'Present',
     dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters',
