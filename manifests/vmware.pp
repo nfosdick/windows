c5c157cccbcd5aba9e82b81d95506dca5e19378d
Nick Fosdick <nick@lark-it.com>
2019-11-01 21:17:06 -0600

    fixing module

--- manifests/vmware.pp
+++ manifests/vmware.pp
@@ -1,11 +1,15 @@
-class windows::vmware {
-  $source_dir='c:\larktemp\VMWare Tools'
-  $vm_tools_dir='c:\temp\VM-Tools'
+class windows::vmware (
+  $logoutput    = 'on_failure',
+  $source_dir   ='c:\larktemp\VMWare Tools',
+  $vm_tools_dir ='c:\temp\VM-Tools',
+){
  
   # https://lark-it.atlassian.net/browse/FCB-152
   exec { 'Copy VMware Tools to C:\Temp':
-    command  => "Copy-Item -Path \"${source_dir}\" -Destination \"${vm_tools_dir}\" -Recurse",
-    provider => powershell,
+    command   => "Copy-Item -Path \"${source_dir}\" -Destination \"${vm_tools_dir}\" -Recurse -Force",
+    provider  => powershell,
+    logoutput => $logoutput,
+    # onlyif or unless "if(command to run if to check if vmware tools is installed){ exit 0 }else{ exit 1 }",
     #onlyif or unless will be needed to check to see if vmware tools is installed already here.  
     #The azure vm would not let me install vmware tools as it wasn't a vmware vm.  
   }   
@@ -15,14 +19,16 @@ class windows::vmware {
     command     => "${vm_tools_dir}\setup64.exe /S /v\"/qn REBOOT=R\"",
     provider    => powershell,
     subscribe   => Exec[ 'Copy VMware Tools to C:\Temp' ],
+    logoutput   => $logoutput,
     refreshonly => true,
   }
 
   # https://lark-it.atlassian.net/browse/FCB-155
   exec { "Remove ${vm_tools_dir} Folder":
-    command  => "Remove-Item –path ${vm_tools_dir} -force -recurse",
-    onlyif   => "if(Test-Path ${vm_tools_dir}){ exit 0 }else{ exit 1 }",
-    provider => powershell,
-    require  => Exec[ 'Install VMware Tools' ],
+    command   => "Remove-Item –path ${vm_tools_dir} -force -recurse",
+    onlyif    => "if(Test-Path ${vm_tools_dir}){ exit 0 }else{ exit 1 }",
+    provider  => powershell,
+    logoutput => $logoutput,
+    require   => Exec[ 'Install VMware Tools' ],
   }
 }
