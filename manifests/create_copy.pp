02e2ea809c70f5100c20bd5982b1d9f78f1acdeb
Nick Fosdick <nick@lark-it.com>
2019-11-01 21:41:04 -0600

    more updates

--- manifests/create_copy.pp
+++ manifests/create_copy.pp
@@ -10,13 +10,13 @@ class windows::create_copy {
 
   # https://lark-it.atlassian.net/browse/FCB-142
   exec { 'Copy PCW':
-    command   => "Copy-Item -Path \"${pcw_source_dir}\" -Destination \"${$pcw_destination_dir}\" -Recurse -Force",
+    command   => "Copy-Item -Path \"${pcw_source_dir}\" -Destination \"${pcw_destination_dir}\" -Recurse -Force",
     provider  => powershell,
     logoutput => $logoutput,
     require   => Dsc_file[ 'PCW Directory' ],
-    # onlyif or unless "if(command to run if to check if vmware tools is installed){ exit 0 }else{ exit 1 }",
-    #onlyif or unless will be needed to check to see if vmware tools is installed already here.
-    #The azure vm would not let me install vmware tools as it wasn't a vmware vm.
+    # onlyif or unless "if(command to run if to check if PCW tools is installed){ exit 0 }else{ exit 1 }",
+    # onlyif or unless will be needed to check to see if PCW tools is installed already here.
+    # I don't have source files here
   }
 
   # https://lark-it.atlassian.net/browse/FCB-150
@@ -27,10 +27,23 @@ class windows::create_copy {
   }
 
   #https://lark-it.atlassian.net/browse/FCB-143
+  $toolbox_source_dir      = 'c:/larktemp/VMWare Tools'
+  $toolbox_destination_dir = 'c:/Toolbox'
   dsc_file {'Toolbox Directory':
     dsc_ensure          => 'present',
     dsc_type            => 'Directory',
-    dsc_destinationpath => 'c:\\Toolbox',
+    dsc_destinationpath => $toolbox_destination_dir,
+  }
+
+  # https://lark-it.atlassian.net/browse/FCB-144
+  exec { 'Copy Toolbox':
+    command   => "Copy-Item -Path \"${toolbox_source_dir}\" -Destination \"${toolbox_destination_dir}\" -Recurse -Force",
+    provider  => powershell,
+    logoutput => $logoutput,
+    require   => Dsc_file[ 'Toolbox Directory' ],
+    # onlyif or unless "if(command to run if to check if toolbox tools is installed){ exit 0 }else{ exit 1 }",
+    # onlyif or unless will be needed to check to see if toolbox tools is installed already here.
+    # I don't have source files here
   }
 
   # https://lark-it.atlassian.net/browse/FCB-145
