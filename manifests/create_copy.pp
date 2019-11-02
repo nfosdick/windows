2fb80a00fc9654071670becce14811e63875ee3f
Nick Fosdick <nick@lark-it.com>
2019-11-02 12:13:31 -0600

    Logging output

--- manifests/create_copy.pp
+++ manifests/create_copy.pp
@@ -122,7 +122,8 @@ class windows::create_copy(
 
   # https://lark-it.atlassian.net/browse/FCB-165
   exec { 'Run CIS Security Script':
-    command   => "start-process \"cmd.exe\" \"/c ${infosec_destination_dir}/${infosec_file}\"",
+    #command   => "start-process \"cmd.exe\" \"/c ${infosec_destination_dir}/${infosec_file}\"",
+    command   => "${infosec_destination_dir}/${infosec_file}",
     provider  => powershell,
     #logoutput => $logoutput,
     logoutput => true,
