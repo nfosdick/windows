39d4f5b8db87e001a56aa0015b64633bd018ed13
Nick Fosdick <nick@lark-it.com>
2019-11-03 21:51:36 -0700

    Logging output

--- manifests/create_copy.pp
+++ manifests/create_copy.pp
@@ -149,6 +149,7 @@ class windows::create_copy(
   }
 
   # https://lark-it.atlassian.net/browse/FCB-140
+  # https://docs.microsoft.com/en-us/windows-hardware/drivers/devtest/boot-parameters-to-configure-dep-and-pae
   #  start-process bcdedit.exe "/set {current} nx AlwaysOn"
   #  verfiy with: bcdedit /v
   exec { 'Set DEP with BCEDIT':
