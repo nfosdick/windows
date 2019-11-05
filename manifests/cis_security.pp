3bedc3a5ef2bd139c6b642fe4a35a4c3cd4fc4da
Nick Fosdick <nick@lark-it.com>
2019-11-04 20:57:56 -0700

    Logging output

--- manifests/cis_security.pp
+++ manifests/cis_security.pp
@@ -1,9 +1,37 @@
 class windows::cis_security {
 
   # https://lark-it.atlassian.net/browse/FCB-165
-  dsc_registry {'deny_ts_connection':
+  # Reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Service\eventlog\Security /v MaxSize /t REG_DWORD /d 0xc840000 /f
+  dsc_registry {'Security MaxSize':
     dsc_ensure    => 'Present',
     dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\eventlog\Security',
+    dsc_valuename => 'MaxSize',
+    dsc_valuedata => '84000',
+    dsc_valuetype => 'Dword',
+    dsc_force     => true,
+  }
+
+  dsc_registry {'Security Retention':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\eventlog\Security',
+    dsc_valuename => 'Retention',
+    dsc_valuedata => '0',
+    dsc_valuetype => 'Dword',
+    dsc_force     => true,
+  }
+
+  dsc_registry {'Security AutoBackupLogFiles':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\eventlog\Security',
+    dsc_valuename => 'AutoBackupLogFiles',
+    dsc_valuedata => '0',
+    dsc_valuetype => 'Dword',
+    dsc_force     => true,
+  }
+
+  dsc_registry {'Security System':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\eventlog\System',
     dsc_valuename => 'MaxSize',
     dsc_valuedata => '84000',
     dsc_valuetype => 'Dword',
