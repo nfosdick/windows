61707e0a27a383201d20f54f58dd9c96fce705f7
Nick Fosdick <nick@lark-it.com>
2019-11-05 18:27:46 -0700

    Logging output

--- manifests/cis_security.pp
+++ manifests/cis_security.pp
@@ -99,4 +99,9 @@ class windows::cis_security {
   }
 
 
-}
+  dsc_registry {'W32Time Default':
+    dsc_ensure    => 'Present',
+    dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers',
+    dsc_valuedata => '6',
+    dsc_force     => true,
+  }}
