85924441cb09e97114d1db93f6afeb55f111af12
Nick Fosdick <nick@lark-it.com>
2019-11-16 11:16:03 -0700

    testing guid

--- files/check_netbios_tcpip.ps1
+++ files/check_netbios_tcpip.ps1
@@ -5,8 +5,10 @@ $key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
 Get-ChildItem $key |
   foreach { 
     $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
+    Write-Host("NetbiosOptions updated value is $($_.pschildname) $NetbiosOptions_Value")
     If ($NetbiosOptions_Value -ne 2) {
-      exit 1;
+      #exit 1;
+      echo hello
     }
     #Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
 }
