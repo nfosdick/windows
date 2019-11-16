bdd26d31762e3ab68cfa6acf51bb40ba720dff5f
Nick Fosdick <nick@lark-it.com>
2019-11-16 00:45:43 -0700

    testing new value

--- files/check_netbios_tcpip.ps1
+++ files/check_netbios_tcpip.ps1
@@ -1,6 +1,12 @@
-$interface  = 'HKLM:\SYSTEM\CurrentControlSet\Services\netbt\Parameters\interfaces'
-  Get-ChildItem $i | ForEach-Object {
-  if( Get-ItemProperty -Path "$i\$($_.pschildname)" -name NetBiosOptions | where{$_.NetBiosOptions -ne 2}){
-    exit 1
-  }
+# http://blog.dbsnet.fr/disable-netbios-with-powershell
+
+$key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
+
+Get-ChildItem $key |
+  foreach { 
+    $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
+    If ($NetbiosOptions_Value -ne 2) {
+      exit 1;
+    }
+    #Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
 }
