ec2f6a14e1a287168c29e90b334acbbe838f9f81
Nick Fosdick <nick@lark-it.com>
2019-11-21 19:30:02 -0700

    minor update

--- manifests/java.pp
+++ manifests/java.pp
@@ -7,7 +7,8 @@ class windows::java(
   dsc_xremotefile {'Download WebContent Zip':
 #    dsc_destinationpath => "${destination_path}\\jdk-${install_version}-windows-x${architecture}.exe',
     dsc_destinationpath => 'c:\larktemp\jdk-8u231-windows-x64.exe',
-    dsc_uri             => "https://s3.amazonaws.com/weslson.com/jdk-${install_version}-windows-x${architecture}.exe",
+    dsc_uri             => 'https://s3.amazonaws.com/weslson.com/jdk-8u231-windows-x64.exe',
+    #dsc_uri             => "https://s3.amazonaws.com/weslson.com/jdk-${install_version}-windows-x${architecture}.exe",
   }
 
 }
