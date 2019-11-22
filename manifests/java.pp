fcaa07765589e5e98417c7f62d0969155544fab6
Nick Fosdick <nick@lark-it.com>
2019-11-21 20:44:52 -0700

    minor update

--- manifests/java.pp
+++ manifests/java.pp
@@ -20,12 +20,12 @@ class windows::java(
 #  }
 
   # https://stackoverflow.com/questions/18890926/how-to-get-the-java-version-in-powershell
-
+  # https://github.com/counsyl/puppet-windows/blob/master/manifests/java.pp
   exec { "Install jdk-${install_version}-windows-${architecture}.exe":
     #command  => "${destination_path}/jdk-${install_version}-windows-${architecture}.exe /s",
     command  => "Start-Process -FilePath ${destination_path}/jdk-${install_version}-windows-${architecture}.exe -ArgumentList '/s' -Wait",
     #unless   => 'if(Get-Command java | Select-Object Version|Select-String 8.0.2310.11){ exit 0 }else{ exit 1 }',
-    #onlyif   => 'Get-Command java | Select-Object Version|Select-String 8.0.2310.11',
+    onlyif   => 'Get-Command java | Select-Object Version|Select-String 8.0.2310.11',
     provider => powershell,
   }
 }
