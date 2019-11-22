b638eb2cea0c070b7d449041164f576ff6c6b56c
Nick Fosdick <nick@lark-it.com>
2019-11-21 19:25:40 -0700

    minor update

--- manifests/java.pp
+++ manifests/java.pp
@@ -5,7 +5,7 @@ class windows::java(
 ){
 
   dsc_xremotefile {'Download WebContent Zip':
-    dsc_destinationpath => "${destination_path}\jdk-${install_version}-windows-x${architecture}.exe',
+    dsc_destinationpath => "${destination_path}\\jdk-${install_version}-windows-x${architecture}.exe',
     dsc_uri             => "https://s3.amazonaws.com/weslson.com/jdk-${install_version}-windows-x${architecture}.exe",
   }
 
