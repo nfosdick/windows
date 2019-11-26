ec89c2fb4b5615d94ab55b45aeb749aa91c81e95
Nick Fosdick <nick@lark-it.com>
2019-11-25 21:03:12 -0700

    Adding comments

--- manifests/java.pp
+++ manifests/java.pp
@@ -21,6 +21,11 @@ class windows::java(
 
   # https://stackoverflow.com/questions/18890926/how-to-get-the-java-version-in-powershell
   # https://github.com/counsyl/puppet-windows/blob/master/manifests/java.pp
+  # https://puppet.com/docs/puppet/5.5/resources_package_windows.html
+  # https://skarlso.github.io/2015/06/30/powershell-can-also-be-nice-or-installing-java-silently-and-waiting/
+  # https://puppet.com/docs/puppet/latest/type.html#package
+  # https://stackoverflow.com/questions/24430141/downloading-jdk-using-powershell
+  # https://github.com/cyberious/puppet-windows_java/blob/master/manifests/jdk.pp
   exec { "Install jdk-${install_version}-windows-${architecture}.exe":
     command  => "Start-Process -FilePath ${destination_path}/jdk-${install_version}-windows-${architecture}.exe -ArgumentList '/s' -Wait",
     unless   => 'if(Get-Command java | Select-Object Version|Select-String 8.0.2310.11){ exit 0 }else{ exit 1 }',
